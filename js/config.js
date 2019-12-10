654c82c34523762647120944c0866368e7ebb2af
EC2 Default User <ec2-user@ip-172-31-26-229.us-east-2.compute.internal>
2019-12-10 17:34:25 +0000

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_A6IPU9dkv', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3r7p04efjq7tl92s61hqqmac03', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
